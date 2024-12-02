b984f38bb1fc4512b936c91ef0ddf86e2ddb4dc7
IvanMagda <ivanmagda164@gmail.com>
2024-12-02 20:14:13 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ivan Magda on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
